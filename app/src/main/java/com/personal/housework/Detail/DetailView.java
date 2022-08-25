e55fd51cd4462e0dc7a4b3d9fa4fcbedc5f7e649
keonwookim <walk000183@gmail.com>
2022-08-26 02:29:25 +0900

    22.08.25.(목) - 액션바 뒤로가기 버튼 기능 추가 - CRUD INSERT 시작하기.

--- app/src/main/java/com/personal/housework/Detail/DetailView.java
+++ app/src/main/java/com/personal/housework/Detail/DetailView.java
@@ -1,8 +1,10 @@
 package com.personal.housework.Detail;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -24,39 +26,21 @@ public class DetailView extends AppCompatActivity {
     EditText et_clothName, et_clothDesc;
     ImageView iv_clothPhoto;
 
-    // 저장된 데이터 불러오기.
-    List<HouseWork> myHouseWorkList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
 
         et_clothName = findViewById(R.id.et_clothName); // 가정일 이름.
+        iv_clothPhoto = findViewById(R.id.iv_clothPhoto); // 가정일 관련 사진.
         et_clothDesc = findViewById(R.id.et_clothDesc); // 가정일 설명하는 부분.
 
-        iv_clothPhoto = findViewById(R.id.iv_clothPhoto); // 가정일 관련 사진.
+        getSupportActionBar().setTitle(""); // 액션바 Title 없애기.
 
 
         // Detail_View 의 속성
         // 1. 양식에 맞게 (데이터베이스에 저장된) 데이터를 불러온다.
         // 2. 양식에 맞게 (데이터베이스에 알맞게) 데이터를 추가한다.
 
-        myHouseWorkList = new ArrayList<>();
-
-        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-        Call<List<HouseWork>> call = service.getAllCloth();
-
-        call.enqueue(new Callback<List<HouseWork>>() {
-            @Override
-            public void onResponse(Call<List<HouseWork>> call, Response<List<HouseWork>> response) {
-                myHouseWorkList = response.body();
-            }
-
-            @Override
-            public void onFailure(Call<List<HouseWork>> call, Throwable t) {
-
-            }
-        });
     }
 }
